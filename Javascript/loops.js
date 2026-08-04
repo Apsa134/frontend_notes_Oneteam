8b5aaf850eb047afe9f322d198ee8bd2097841ce
ApsaBiju <apsabiju04@gmail.com>
2026-08-04 09:16:25 +0530

    Javascript

--- Javascript/loops.js
+++ Javascript/loops.js
@@ -20,15 +20,42 @@
 
 // console.log(factorial(5));
 
-//fibanocci series
-let a = 0 , b =1
-console.log(a , b)
-for (let j =0;j<7;j++){
-	c = a+b;
-	a = b
-	b=c
-	console.log(c)
+// //fibanocci series
+// let a = 0 , b =1
+// console.log(a , b)
+// for (let j =0;j<7;j++){
+// 	c = a+b;
+// 	a = b
+// 	b=c
+// 	console.log(c)
+// }
+
+// prime number
+function prime(){
+	 let num = prompt("Enter a number")
+	is_prime = true
+	for ( let i =2;i<num; i++){
+		if(num%i==0){
+			is_prime = false
+			alert("Not prime!!!")
+			return
+		}
+
+	}
+	if(is_prime){
+		alert("Prime!!!")
+		return
+	}
 }
 
-// pattern print
- 
+let i =1
+do{
+	console.log(i)
+	i++
+}while(i<5)
+
+	//array
+let arr=[1,3,5,7,9]
+for(let i=0;i<arr.length;i++){
+	console.log(arr[i])
+}
